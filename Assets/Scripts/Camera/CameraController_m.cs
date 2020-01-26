ef22f945981aca0d4718a1d75a3e1306b6a27bf9
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-01-26 14:00:59 -0430

    Animaciones

---
+++ Assets/Scripts/Camera/CameraController_m.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController_m : MonoBehaviour 
+{
+	public Transform Player;
+	public float m_speed = 0.1f;
+	public void Start()
+	{
+
+	}
+
+	public void Update()
+	{
+
+
+		if (Player) 
+		{
+		
+			transform.position = Vector3.Lerp(transform.position, Player.position, m_speed) + new Vector3(0, 0, -12);
+		}
+
+
+	}
+}
