4dd11006c5b622e7e6f38d40540bfd9caa4b3658
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-01-26 14:44:39 -0430

    De estado parado a caminando

--- Assets/Scripts/Player/Control.cs
+++ Assets/Scripts/Player/Control.cs
@@ -9,23 +9,52 @@ public class Control : MonoBehaviour
 	public float runSpeed = 2f;
 
 	private SpriteRenderer spriteRenderer;
-	public bool isFlipped = false;
+	private Rigidbody2D body;
+
+	private bool isFlipped = false;
 	private bool isGrounded = false;
+	private float horizontalMovement = 0f;
+	private float verticalMovement = 0f;
+	private Animator animator;
 	// Use this for initialization
 	void Start () 
 	{
 		this.spriteRenderer = this.GetComponent<SpriteRenderer> ();
+		this.body = this.GetComponent<Rigidbody2D> ();
+		this.animator = this.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		float horizontalMovement = CrossPlatformInputManager.GetAxis("Horizontal");
+		this.horizontalMovement = CrossPlatformInputManager.GetAxis("Horizontal");
+		this.Flip ();
+		this.Walk ();
+	}
+
+	void Walk()
+	{
+		this.animator.SetFloat ("velocityX", Mathf.Abs(this.body.velocity.x));
+	}
+
+	void Run()
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		Vector2 movement = this.body.velocity; 
+		movement.x = this.horizontalMovement * this.walkSpeed;
+		this.body.velocity = movement;
 	}
 
 	void Flip()
 	{
-		this.isFlipped = !this.isFlipped;
-		this.spriteRenderer.flipX = this.isFlipped;
+		if (this.isFlipped && this.horizontalMovement > 0 || !this.isFlipped && this.horizontalMovement < 0) 
+		{
+			this.isFlipped = !this.isFlipped;
+			this.spriteRenderer.flipX = this.isFlipped;
+		}
 	}
 }
