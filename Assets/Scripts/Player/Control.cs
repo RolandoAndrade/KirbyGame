c947940a3154c896897cdd24f35f903862f6c96e
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-01-26 14:18:57 -0430

    Añadida la entrada multiplataforma

--- Assets/Scripts/Player/Control.cs
+++ Assets/Scripts/Player/Control.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class Control : MonoBehaviour {
+public class Control : MonoBehaviour 
+{
+	public float walkSpeed = 1f;
+	public float runSpeed = 2f;
 
+	private SpriteRenderer spriteRenderer;
+	public bool isFlipped = false;
+	private bool isGrounded = false;
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		this.spriteRenderer = this.GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		float horizontalMovement = CrossPlatformInputManager.GetAxis("Horizontal");
+	}
+
+	void Flip()
+	{
+		this.isFlipped = !this.isFlipped;
+		this.spriteRenderer.flipX = this.isFlipped;
 	}
 }
