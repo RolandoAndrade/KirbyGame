86a60965c56ab568f9de25b9c512943a14e93dd4
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-04-16 16:20:50 -0430

    Basic state factory

--- Assets/Scripts/Factory/ActionsFactory.cs
+++ Assets/Scripts/Factory/ActionsFactory.cs
@@ -1,6 +1,10 @@
 ﻿public interface ActionsFactory
 {
-	PlayerAction GetJump();
-	PlayerAction GetDown();
-	PlayerAction GetEat();
+	PlayerAction GetJump(Control player);
+	PlayerAction GetDown(Control player);
+	PlayerAction GetEat(Control player);
+	PlayerAction GetFly(Control player);
+	PlayerAction GetLand(Control player);
+	PlayerAction GetRun(Control player);
+	PlayerAction GetWalk(Control player);
 }
---
+++ Assets/Scripts/Factory/BasicActionFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class BasicActionFactory
+{
+	public PlayerAction GetJump(Control player)
+	{
+		return new Jump (player);
+	}
+
+	PlayerAction GetDown(Control player)
+	{
+		return new Down (player);
+	}
+
+	PlayerAction GetEat(Control player)
+	{
+		return new Eat (player);
+	}
+
+	PlayerAction GetFly(Control player)
+	{
+		return new Fly (player);
+	}
+
+	PlayerAction GetLand(Control player)
+	{
+		return new Land (player);
+	}
+
+	PlayerAction GetRun(Control player)
+	{
+		return new Run (player);
+	}
+
+	PlayerAction GetWalk(Control player)
+	{
+		return new Walk (player);
+	}
+}
+
