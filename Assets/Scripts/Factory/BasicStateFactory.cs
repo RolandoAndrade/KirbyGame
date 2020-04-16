10d8ee8baf0c9821e1b1d0e864dc7bc7bc55a990
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-04-16 11:13:30 -0430

    Agregada fábrica

---
+++ Assets/Scripts/Factory/BasicStateFactory.cs
@@ -0,0 +1,27 @@
+﻿public class BasicStateFactory : StateFactory 
+{
+	public PlayerState GetNormalState(Control player)
+	{
+		return new NormalState (player);
+	}
+
+	public PlayerState GetJumpState(Control player)
+	{
+		return new JumpState (player);
+	}
+		
+	public PlayerState GetDownState(Control player)
+	{
+		return new DownState (player);
+	}
+
+	public PlayerState GetEatingState(Control player)
+	{
+		return new EatingState (player);
+	}
+
+	public PlayerState GetFlyingState(Control player)
+	{
+		return new FlyingState (player);
+	}
+}
---
+++ Assets/Scripts/Factory/StateFactory.cs
@@ -0,0 +1,8 @@
+﻿public interface StateFactory 
+{
+	PlayerState GetNormalState(Control player);
+	PlayerState GetJumpState(Control player);
+	PlayerState GetDownState(Control player);
+	PlayerState GetEatingState(Control player);
+	PlayerState GetFlyingState(Control player);
+}
