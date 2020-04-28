b5fe7fef2716c64ac5be25be33ebb9e14bec69ff
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-04-27 21:28:11 -0430

    Música entre niveles

--- Assets/Scripts/Listeners/PrologGameListener.cs
+++ Assets/Scripts/Listeners/PrologGameListener.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PrologGameListener : MonoBehaviour, GameListener 
 {
+	public MusicClass music;
 	public Captions captions;
+	private bool played = false;
 
 	public bool Listen ()
 	{
+		if (captions.IsEnded ()&&!played)
+		{
+			played = true;
+			music.ChangeTrack ();
+		}
 		return captions.IsEnded ();
 	}
 }
---
+++ Assets/Scripts/Music/MusicClass.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicClass : MonoBehaviour
+{
+	public float fadeTime = 1f;
+	private AudioSource[] traks;
+	private int trak = 0;
+	private bool changeTrak = false;
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+		this.traks = this.GetComponents<AudioSource> ();
+		Debug.Log (traks.Length);
+	}
+
+	void Update()
+	{
+	}
+
+	public void PlayMusic()
+	{
+		if (traks[trak].isPlaying) return;
+		traks[trak].Play();
+	}
+
+	public void StopMusic()
+	{
+		traks[trak].Stop();
+	}
+
+	public void ChangeTrack()
+	{
+		if (!this.changeTrak)
+		{
+			this.changeTrak = true;
+			StartCoroutine (FadeMusic ());
+		}
+
+	}
+
+	IEnumerator FadeMusic()
+	{
+		if (!traks [trak + 1].isPlaying)
+			traks [trak + 1].Play ();
+		while (traks[trak].volume > 0) 
+		{
+			traks [trak].volume -= Time.deltaTime / fadeTime;
+			traks [trak + 1].volume += Time.deltaTime / fadeTime;
+			yield return null;
+		}
+		traks [trak].Stop ();
+		this.trak++;
+		this.changeTrak = false;
+
+	}
+}
